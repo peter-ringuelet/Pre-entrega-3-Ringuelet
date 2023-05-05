cd4ebc0b114c081eaf97e7c8d0242384b5936793
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-05 00:41:32 -0300

    Hasta clase 18

--- ProyectoSACCOM/AppSACCOM/apps.py
+++ ProyectoSACCOM/AppSACCOM/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppsacommConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppSACOMM'
+    name = 'AppSACCOM'
--- ProyectoSACCOM/AppSACCOM/models.py
+++ ProyectoSACCOM/AppSACCOM/models.py
@@ -8,4 +8,10 @@ class ComisionDirectiva(models.Model):
     cargo = models.CharField(max_length=30)
 
 class Socio(models.Model):
-    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    dni = models.IntegerField()
+
+class Actividad(models.Model):
+    nombre = models.CharField(max_length=30)
+    fecha_inicio = models.DateField()
