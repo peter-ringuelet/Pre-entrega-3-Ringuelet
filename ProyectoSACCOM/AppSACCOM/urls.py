7f3222202dfea6624ff9854a1fef99636ef058a9
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-06 02:30:51 -0300

    Terminado

--- ProyectoSACCOM/AppSACCOM/urls.py
+++ ProyectoSACCOM/AppSACCOM/urls.py
@@ -6,9 +6,5 @@ urlpatterns = [
     path('comision', views.comision, name='Comision'),
     path('socios', views.socio, name="Socios"),
     path('actividades', views.actividad, name="Actividades"),
-    #path('actividadFormulario', views.actividadFormulario, name="ActividadFormulario"),
-    #path('comisionFormulario', views.comisionFormulario, name="ComisionFormulario"),
-    #path('socioFormulario', views.socioFormulario, name="SocioFormulario"),
-    #path('busquedaActividad', views.busquedaActividad, name="BusquedaActividad"),
     path('buscar/', views.buscar),
 ]
