2da3a5de3b3c25cc067d99aed9cbc9bbaca3f8d6
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-07 23:05:01 -0300

    Revert "Espero no se cambie en la entrega"
    This reverts commit b1ea28937d549914c6c407baca2b582c80d71775.

--- ProyectoSACCOM/AppSACCOM/models.py
+++ ProyectoSACCOM/AppSACCOM/models.py
@@ -6,19 +6,12 @@ class ComisionDirectiva(models.Model):
     apellido = models.CharField(max_length=30)
     dni = models.IntegerField()
     cargo = models.CharField(max_length=30)
-    def __str__(self) -> str:
-        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - DNI: {self.dni} - Cargo: {self.cargo}"
 
 class Socio(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     dni = models.IntegerField()
-    def __str__(self) -> str:
-        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - DNI: {self.dni}"
 
 class Actividad(models.Model):
     nombre = models.CharField(max_length=30)
     fecha_inicio = models.DateField()
-    def __str__(self) -> str:
-        return f"Nombre: {self.nombre} - Fecha Inicio: {self.fecha_inicio}"
-    
